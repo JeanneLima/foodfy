8cf976b619d5abfd035c24b74d98c896f6825965
Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>
2021-12-23 18:33:23 -0300

    feat(recipes page): add logic to show respective content on each recipe modal

--- web/scripts/recipes.js
+++ web/scripts/recipes.js
@@ -5,18 +5,14 @@ for (let recipeCard of recipeCardsList) {
   recipeCard.addEventListener('click', () => {
     const recipeImageFileName = recipeCard.getAttribute('id').split("--")[1];
     const recipeTitle = recipeCard.getElementsByClassName('recipes__list-item-title')[0].firstChild.nodeValue;
-    const recipeAuthor = recipeCard.getElementsByClassName('recipes__list-item-author')[0].firstChild.nodeValue
+    const recipeAuthor = recipeCard.getElementsByClassName('recipes__list-item-author')[0].firstChild.nodeValue;
 
-    console.log('recipeTitle:', recipeTitle)
-
-    document.querySelector('.modal__image').src = `./assets/${recipeImageFileName}.png`;
+    document.querySelector('.modal__image').src = `./../assets/${recipeImageFileName}.png`;
     document.querySelector('.modal__title').firstChild.nodeValue = recipeTitle;
     document.querySelector('.modal__author').firstChild.nodeValue = recipeAuthor;
 
     modalOverlay.classList.add('modal__overlay--active')
   });
-
-
 }
 
 document.querySelector('.modal__link--close').addEventListener('click', () => modalOverlay.classList.remove('modal__overlay--active'));
