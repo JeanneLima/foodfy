bd5220f369fc9984ca7e016e7a62ab8d13d79541
Jeanne Lima <23726423+JeanneLima@users.noreply.github.com>
2022-01-02 17:53:51 -0300

    Develop - Consume data dynamically from data.js file (#16)
    * feat(assets): add assets folder with project images
    
    * Add base structure folders and files
    
    * feat(navbar): Add global navbar structure and style
    
    * style(navbar): Add padding to navbar
    
    * feat(header home): Add header structure and style to home page
    
    * feat(home recipes): add most popular recipes to home page
    
    * feat(home): add footer structure and style
    
    * Feature/readme update (#3)
    
    * docs(readme): add readme basic description
    
    * docs(readme): update readme title
    
    Co-authored-by: Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>
    
    * feat(about): add about page base structure and style
    
    * feat(assets): rename assets files
    
    * feat(assets): delete repeated assets files
    
    * feat(recipes): add recipes page base structure and style
    
    * feat(assets): remove assets folder from the root of the project
    
    * feat(packages): remove packge.json file
    
    * feat(home): add recipe modal structure and style
    
    * style(home): change recipe modal width
    
    * style(home): change recipe modal image height
    
    * feat(home): add base scripts logic to recipe modal
    
    * feat(home): add logic to show respective content on each recipe modal
    
    * feat(recipes page): add logic to show respective content on each recipe modal
    
    * docs(specs): update specs files
    
    * feat(scripts): refactor the recipe modal toogle logic
    
    * feat(utils): refactor toggleRecipeModal function
    
    * docs(readme): update readme challenges logs and adopted practices
    
    * style(linters): add eslint and prettier as code formatting tools
    
    * style(linters): add linters fix to terminal command line (scripts package.json)
    
    * style(linters): force change - add linters fix to terminal command line (scripts package.json)
    
    * feat(server): add server base structure using express
    
    * feat(server): add first route base configuration
    
    * feat(server): configure nodemon to watch server modifications
    
    * feat(about): adapt the about page to the format of nunjucks
    
    * feat(about page): change about page main content id
    
    * feat(nunjucks): disable template engine cache
    
    * feat(home page): adapt the home page to the format of nunjucks
    
    * feat(recipes page): adapt the recipes page to the format of nunjucks
    
    * feat(recipes page): adjust recipes page stylesheet imported
    
    * feat(routes): change the path of routes
    
    * feat(routes): change anchors/href from global navbar
    
    * feat(data): add recipes data via json file
    
    * style(linters): change eslint import/extensions rule
    
    * feat(recipes page): consume recipes data from data.js file
    
    * feat(home page): consume all recipes data from data.js file
    
    * feat(home page): show only the first 6 recipes on the home page
    
    * style(linters): change eslint arrow-parens rule
    
    * Merge branch 'main' into develop
    
    * feat(assets): remove recipes images from assets folder
    
    * fix(merge): fix merge problems
    
    Co-authored-by: Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -1,23 +1,35 @@
-export const insertRecipeModalContent = (recipeImageFileName, recipeTitle, recipeAuthor, recipeAssetsDir) => {
-  document.querySelector('.modal__image').src = `${recipeAssetsDir + recipeImageFileName}.png`;
+export const insertRecipeModalContent = (
+  recipeImageFileName,
+  recipeTitle,
+  recipeAuthor,
+) => {
+  document.querySelector('.modal__image').src = `${recipeImageFileName}`;
   document.querySelector('.modal__title').firstChild.nodeValue = recipeTitle;
   document.querySelector('.modal__author').firstChild.nodeValue = recipeAuthor;
-}
+};
 
-export const showRecipeModal = (modalOverlay, recipeCardsList, recipeAssetsDir) => {
-  for (let recipeCard of recipeCardsList) {
+export const showRecipeModal = (modalOverlay, recipeCardsList) => {
+  for (const recipeCard of recipeCardsList) {
     recipeCard.addEventListener('click', () => {
-      const recipeImageFileName = recipeCard.getAttribute('id').split("--")[1];
-      const recipeTitle = recipeCard.getElementsByClassName('recipes__list-item-title')[0].firstChild.nodeValue;
-      const recipeAuthor = recipeCard.getElementsByClassName('recipes__list-item-author')[0].firstChild.nodeValue;
-  
-      insertRecipeModalContent(recipeImageFileName, recipeTitle, recipeAuthor, recipeAssetsDir);
-  
-      modalOverlay.classList.add('modal__overlay--active')
+      const recipeImageFileName = recipeCard
+        .getElementsByClassName('recipes__list-item-image')[0]
+        .getAttribute('src');
+      const recipeTitle = recipeCard.getElementsByClassName(
+        'recipes__list-item-title',
+      )[0].firstChild.nodeValue;
+      const recipeAuthor = recipeCard.getElementsByClassName(
+        'recipes__list-item-author',
+      )[0].firstChild.nodeValue;
+
+      insertRecipeModalContent(recipeImageFileName, recipeTitle, recipeAuthor);
+
+      modalOverlay.classList.add('modal__overlay--active');
     });
   }
-}
+};
 
 export const hideRecipeModal = (closeTriggerElement, modalOverlay) => {
-  closeTriggerElement.addEventListener('click', () => modalOverlay.classList.remove('modal__overlay--active'));
-}
+  closeTriggerElement.addEventListener('click', () =>
+    modalOverlay.classList.remove('modal__overlay--active'),
+  );
+};
