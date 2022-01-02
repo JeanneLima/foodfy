67803d5f6ec8b4fc4da023125e8430e74b75e767
Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>
2022-01-02 12:05:32 -0800

    feat(about): adapt the about page to the format of nunjucks

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const server = express();
 // Template engine configurations
 server.use(express.static('public'));
 
-server.set('view engine', 'html');
+server.set('view engine', 'njk');
 
 nunjucks.configure('views', { express: server });
 
 // Routes
-server.get('/', (req, res) => res.send('root route'));
+server.get('/sobre', (req, res) => res.render('about'));
 
 // Port configuration
 server.listen('5000', () => {
