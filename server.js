96e0eaf8cc4f85b92883702b7ea05c33a1f05623
Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>
2022-01-02 11:33:50 -0800

    feat(server): add first route base configuration

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const server = express();
 
+// Template engine configurations
+server.use(express.static('public'));
+
+server.set('view engine', 'html');
+
+nunjucks.configure('views', { express: server });
+
+// Routes
+server.get('/', (req, res) => res.send('root route'));
+
+// Port configuration
 server.listen('5000', () => {
   console.log('Server is running');
 });
