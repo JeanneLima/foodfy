2cbd6f9224d7f351c7b1be6e07cac7f73a4cd2fd
Jeanne Lima <23726423+JeanneLima@users.noreply.github.com>
2022-01-03 08:38:43 -0300

    Develop - Add recipe details page (#18)
    * feat(assets): add assets folder with project images
    
    * Add base structure folders and files
    
    * feat(navbar): Add global navbar structure and style
    
    * style(navbar): Add padding to navbar
    
    * feat(header home): Add header structure and style to home page
    
    * feat(home recipes): add most popular recipes to home page
    
    * feat(home): add footer structure and style
    
    * Feature/readme update (#3)
    
    * docs(readme): add readme basic description
    
    * docs(readme): update readme title
    
    Co-authored-by: Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>
    
    * feat(about): add about page base structure and style
    
    * feat(assets): rename assets files
    
    * feat(assets): delete repeated assets files
    
    * feat(recipes): add recipes page base structure and style
    
    * feat(assets): remove assets folder from the root of the project
    
    * feat(packages): remove packge.json file
    
    * feat(home): add recipe modal structure and style
    
    * style(home): change recipe modal width
    
    * style(home): change recipe modal image height
    
    * feat(home): add base scripts logic to recipe modal
    
    * feat(home): add logic to show respective content on each recipe modal
    
    * feat(recipes page): add logic to show respective content on each recipe modal
    
    * docs(specs): update specs files
    
    * feat(scripts): refactor the recipe modal toogle logic
    
    * feat(utils): refactor toggleRecipeModal function
    
    * docs(readme): update readme challenges logs and adopted practices
    
    * style(linters): add eslint and prettier as code formatting tools
    
    * style(linters): add linters fix to terminal command line (scripts package.json)
    
    * style(linters): force change - add linters fix to terminal command line (scripts package.json)
    
    * feat(server): add server base structure using express
    
    * feat(server): add first route base configuration
    
    * feat(server): configure nodemon to watch server modifications
    
    * feat(about): adapt the about page to the format of nunjucks
    
    * feat(about page): change about page main content id
    
    * feat(nunjucks): disable template engine cache
    
    * feat(home page): adapt the home page to the format of nunjucks
    
    * feat(recipes page): adapt the recipes page to the format of nunjucks
    
    * feat(recipes page): adjust recipes page stylesheet imported
    
    * feat(routes): change the path of routes
    
    * feat(routes): change anchors/href from global navbar
    
    * feat(data): add recipes data via json file
    
    * style(linters): change eslint import/extensions rule
    
    * feat(recipes page): consume recipes data from data.js file
    
    * feat(home page): consume all recipes data from data.js file
    
    * feat(home page): show only the first 6 recipes on the home page
    
    * style(linters): change eslint arrow-parens rule
    
    * Merge branch 'main' into develop
    
    * feat(assets): remove recipes images from assets folder
    
    * fix(merge): fix merge problems
    
    * feat(routes): add route to return data from a specific recipe
    
    * feat(scripts): add redirect logic to page with clicked recipe data
    
    * feat(home page): add redirect logic to page with clicked recipe data
    
    * feat(pages): add structure of recipe details page
    
    * style(recipe details page): add styles from recipe details page
    
    * feat(recipe details): add scripts to toggle recipe details content
    
    * feat(scripts): hides the extra info block on the recipe details page when there is no data
    
    * style(recipe details page): change button cursor
    
    Co-authored-by: Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>

---
+++ public/scripts/recipeDetails.js
@@ -0,0 +1,26 @@
+// --- Global variables ---//
+const recipeDetailsCollapseButtonsList = document.querySelectorAll(
+  '.recipe__description-header-control',
+);
+
+// --- General functions ---//
+export const toggleRecipeDetails = () => {
+  for (const recipeDetailsCollapseButton of recipeDetailsCollapseButtonsList) {
+    recipeDetailsCollapseButton.addEventListener('click', () => {
+      const thisContent = recipeDetailsCollapseButton.parentElement.parentElement
+        .querySelector('.recipe__description-content');
+      
+      const thisContentClassesList = thisContent.classList;
+
+      if (thisContentClassesList.contains('recipe__description-content--hidden')) {
+        thisContentClassesList.remove('recipe__description-content--hidden');
+        recipeDetailsCollapseButton.textContent = "Esconder";
+      } else {
+        thisContentClassesList.add('recipe__description-content--hidden');
+        recipeDetailsCollapseButton.textContent = "Mostrar";
+      }
+    });
+  }
+};
+
+toggleRecipeDetails();
