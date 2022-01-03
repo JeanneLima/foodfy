105aca9d334b5e7aec502e6eb0c32480207dc4f9
Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>
2022-01-02 19:30:31 -0800

    feat(home page): add redirect logic to page with clicked recipe data

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -1,45 +1,7 @@
-export const showRecipeDetails = recipeCardsList => {
-  console.log('recipeCardsList:', recipeCardsList);
-
+export const showRecipeDetails = (recipeCardsList) => {
   recipeCardsList.forEach((recipeCard, index) => {
     recipeCard.addEventListener('click', () => {
       window.location.href = `/recipes/${index}`;
     });
   });
-};
-
-export const insertRecipeModalContent = (
-  recipeImageFileName,
-  recipeTitle,
-  recipeAuthor,
-) => {
-  document.querySelector('.modal__image').src = `${recipeImageFileName}`;
-  document.querySelector('.modal__title').firstChild.nodeValue = recipeTitle;
-  document.querySelector('.modal__author').firstChild.nodeValue = recipeAuthor;
-};
-
-export const showRecipeModal = (modalOverlay, recipeCardsList) => {
-  for (const recipeCard of recipeCardsList) {
-    recipeCard.addEventListener('click', () => {
-      const recipeImageFileName = recipeCard
-        .getElementsByClassName('recipes__list-item-image')[0]
-        .getAttribute('src');
-      const recipeTitle = recipeCard.getElementsByClassName(
-        'recipes__list-item-title',
-      )[0].firstChild.nodeValue;
-      const recipeAuthor = recipeCard.getElementsByClassName(
-        'recipes__list-item-author',
-      )[0].firstChild.nodeValue;
-
-      insertRecipeModalContent(recipeImageFileName, recipeTitle, recipeAuthor);
-
-      modalOverlay.classList.add('modal__overlay--active');
-    });
-  }
-};
-
-export const hideRecipeModal = (closeTriggerElement, modalOverlay) => {
-  closeTriggerElement.addEventListener('click', () =>
-    modalOverlay.classList.remove('modal__overlay--active'),
-  );
 };
